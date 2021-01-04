c116262e2f03e9236d1d13c2992a92694d56885c
joaoviniciusrodrigues <32283342+joaoviniciusrodrigues@users.noreply.github.com>
2021-01-04 18:09:30 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/devsuperior/hruser/resorcies/UserResource.java
@@ -0,0 +1,31 @@
+package com.devsuperior.hruser.resorcies;
+
+import com.devsuperior.hruser.entities.User;
+import com.devsuperior.hruser.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RefreshScope
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(repository.findById(id).get());
+
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<User> findById(@RequestParam("email") String email) {
+        return ResponseEntity.ok(repository.findByEmail(email));
+
+    }
+
+}
